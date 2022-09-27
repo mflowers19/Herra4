65cd7844558357054e25c168480a5c4d6c202979
luiss <luissamaniego0117@gmail.com>
2022-09-26 21:10:56 -0500

    Acumulador y Restriccion de votacion nuevamente

--- app/src/main/java/com/example/herra4_proyecto1/VariablesGlobales.java
+++ app/src/main/java/com/example/herra4_proyecto1/VariablesGlobales.java
@@ -4,7 +4,8 @@ public class VariablesGlobales {
     public static float votoc1;
     public static float votoc2;
     public static float votoc3;
-
+   public static String[] vectorCedulas = {"8-944-327", "3-740-1394", "20-53-4282", "8-943-1867", "8-937-503", "8-952-2444", "8-943-12", "8-986-549", "8-957-1827", "8-940-1311", "8-863-1620", "8-964-691", "8-954-1126", "8-1039-1537", "8-925-1168", "8-940-1505", "8-938-925", "3-743-2493", "E-8-169755", "8-971-1811", "8-940-408", "8-937-1640", "8-939-1578", "8-942-1406", "6-722-724", "8-941-1215", "8-962-1218", "8-941-924","8-980-2414", "3-742-2055", "8-942-1192", "8-904-1189", "8-959-857", "8-964-1554", "8-1066-1018", "8-1055-701", "8-970-242", "3-752-1461", "3-745-950"};
+    public static String[] vectorNombres = {"Edwin Arrocha", "Michelle Brenes", "Eddy Bustamante", "Alejandra Caballero", "Gabriel Chiari", "Kevin Coronado", "Luis Delgado", "Marc Dudley", "Diego Fernandez", "María Flores", "Donaldo Frazer", "Miguel Gascón", "Andrew Gonzalez", "Valerie Hernández", "Tommy Ho", "Jean Jiménez", "Felipe Loo", "Lisseth Mcdonald", "Jesus Monagas", "Diana Moreno", "Gerardo Moreno", "Juan Navarro", "Julio Navas", "Anel Pinzón", "Janiel Pinzón", "Antonio Qiu", "Carlos Ricord", "David Rios", "Sergio Rodriguez", "Zulixa Salas", "Luis Samaniego", "Ary Sánchez", "Dionisio Santos", "Bryan Thomas", "Carlos Tuñón", "Genesis Vitale", "Fernando Zeng", "Carlos Zhong", "Eimy Guerra"};
 
     public static float getVotoc1() {
         return votoc1;
